1b8c903e56a9d212b5ecc564088ebf827ebbebd8
vicabats <vitoria.abreu@caju.com.br>
2025-06-03 21:01:40 -0300

    feat: create DS tokens

---
+++ mobile/core/design-system/tokens/colors.js
@@ -0,0 +1,15 @@
+const colors = {
+  primary: "#43a06c",
+  primaryDark: "#388E3C",
+  secondary: "#FFC107",
+  tertiary: "#FFFFFF",
+  error: "#E53935",
+  success: "#43A047",
+  warning: "#FBC02D",
+  white: "#FFFFFF",
+  black: "#000000",
+  gray: "#c5c5c5",
+  darkerGray: "#524d4d",
+};
+
+export default colors;
