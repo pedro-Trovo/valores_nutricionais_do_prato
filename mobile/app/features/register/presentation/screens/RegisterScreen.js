1a85d92c91017cef8c0b932bf151d5a7ddd1f073
vicabats <vitoria.abreu@caju.com.br>
2025-06-03 21:03:10 -0300

    feat: create RegisterScreen

---
+++ mobile/app/features/register/presentation/screens/RegisterScreen.js
@@ -0,0 +1,21 @@
+import AppText from "design-system/components/AppText";
+import Header from "design-system/components/Header";
+import { View } from "react-native";
+import sizes from "design-system/tokens/sizes";
+import colors from "design-system/tokens/colors";
+import { useNavigation } from "@react-navigation/native";
+
+export default function RegisterScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={{ flex: 1, backgroundColor: colors.white }}>
+      <Header onBack={() => navigation.goBack()} />
+      <View style={{ flex: 1, alignItems: "center" }}>
+        <AppText fontSize={sizes.lg} color={colors.darkerGray}>
+          Register Screen
+        </AppText>
+      </View>
+    </View>
+  );
+}
