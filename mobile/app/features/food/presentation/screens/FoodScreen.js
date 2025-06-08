b0c7d80851ed9f4eda2b933a84de353ce7b0e8e2
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-06-08 20:02:51 -0300

    feat: cria o componente do headertwo

---
+++ mobile/app/features/food/presentation/screens/FoodScreen.js
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import HeaderTwo from '../../../../../core/design-system/components/HeaderTwo';
+
+export default function FoodScreen() {
+  return (
+    <View>
+      <HeaderTwo />
+    </View>
+  );
+}
